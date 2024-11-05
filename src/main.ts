687428a6c26b52dc6c607e4f8e5d5c43bba38b1a
Tarae0419 <tarae0419@gmail.com>
2024-11-06 00:49:56 +0900

    파일 추가

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,13 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { provideRouter } from '@angular/router';
+import { importProvidersFrom } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { routes } from './app/app.routes';
 
-createApp(App).use(store).use(router).mount("#app");
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(FormsModule)
+  ]
+}).catch(err => console.error(err));
